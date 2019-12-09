be9f4319df1f83e22bb8f7f496660b764a97706e
= <=>
2019-12-09 16:15:13 +0400

    Routing maybe has completed.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import { Route, NavLink } from 'react-router-dom';
+// import NewPost from './NewPost/NewPost';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import AsyncComponent from '../hoc/asyncComponent';
+
+const AsyncNewPost = AsyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+    state = {
+        userAuth: true
+    }
     render() {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink to='/' exact>Home</NavLink></li>
+                            <li><NavLink to='/posts'
+                                exact
+                                activeClassName='my-active'
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -20,8 +34,14 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path='/' exact component={Posts}/>
-                <Route path='/new-post' component={NewPost}/>
+                <Switch>
+                    {this.state.userAuth && <Route path='/new-post' component={AsyncNewPost} />}
+                    <Route path='/posts' component={Posts} />
+                    <Route render={() => <h1>Not Found</h1>}/>
+             {/* Yuxaridaki route ile redirect biryerde isleye bilmez cunki her ikisi butun yalnisadresleri tutur */}
+                    {/* <Redirect from='/' to='/posts' /> */}
+                    {/* <Route path='/' component={Posts} /> */}
+                </Switch>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Post from '../../../components/Post/Post';
 import axios from '../../../axios';
 import './Posts.css';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import { Route } from 'react-router-dom';
+import FullPost from '../FullPost/FullPost';
 
 
 class Posts extends Component {
@@ -11,36 +14,40 @@ class Posts extends Component {
     }
 
     selectedPostHandler = (id) => {
-        this.setState({selectedPostId: id});
+        this.setState({ selectedPostId: id });
+        // this.props.history.push({pathname: '/' + id});
+        this.props.history.push('/posts/' + id);
     }
 
     componentDidMount() {
         axios('/posts')
             .then(response => {
-                const posts = response.data.slice(0,4);
+                const posts = response.data.slice(0, 4);
                 const updatePosts = posts.map(post => {
                     return {
                         ...post,
                         author: 'Max'
                     }
                 });
-                this.setState({posts: updatePosts});
+                this.setState({ posts: updatePosts });
             });
     }
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.selectedPostHandler(post.id)} />
-        });
+        let posts = this.state.posts.length === 0 ? <Spinner /> :
+            this.state.posts.map(post => {
+                return <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.selectedPostHandler(post.id)} />
+            });
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost}/>
             </div>
         )
     }
